cde1705744cd78ab4b63ffefd1b02a727b7c75ef
hideki1618 <anderson.yamamura@yahoo.com.br>
2025-03-03 11:58:44 -0300

    Fix schedule to ignore canceled streams

--- utils/twitch_api.py
+++ utils/twitch_api.py
@@ -5,25 +5,34 @@ from twitchAPI.type import TwitchResourceNotFound
 from utils.helpers import timestamp_discord
 
 async def get_twitch_schedule(channel_id,schedule_limit):
-    # initialize the twitch instance, this will by default also create a app authentication for you
+    """Get the schedule of a twitch channel in discord timestamp format"""
+    # initialize the twitch instance
     twitch = await Twitch(TWITCH_CLIENT_ID, TWITCH_ACCESS_TOKEN)
     
-    # call the API for the data of your twitch user
-    # this returns a async generator that can be used to iterate over all results
-    # but we are just interested in the first result
-    # using the first helper makes this easy.
+    # get the channel's stream schedule
     try:
-        response = await twitch.get_channel_stream_schedule(channel_id,first=schedule_limit)
+        response = await twitch.get_channel_stream_schedule(channel_id)
     except TwitchResourceNotFound:
         return None
     
+    # iterate through the schedule and return the first `schedule_limit` entries that are not canceled
+    schedule_count = 0
     schedule_return = []
-    for segment in response.segments:
+    async for segment in response:
+        if segment.canceled_until is not None:
+            continue
         schedule_return.append(timestamp_discord(segment.start_time))
+        schedule_count += 1
+        if schedule_count == schedule_limit:
+            break
+    
+    # close the twitch instance
     await twitch.close()
+
     return "\n".join(schedule_return)
 
 async def get_twitch_user_id(channel):
+    """Get the user id and display name of a twitch channel"""
     twitch = await Twitch(TWITCH_CLIENT_ID, TWITCH_ACCESS_TOKEN)
     user = await first(twitch.get_users(logins=channel))
     await twitch.close()
